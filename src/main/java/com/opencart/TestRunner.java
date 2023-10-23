32ca41f5f32badb511479fca69a71d866a1319a5
nadia <nandiny.bors@gmail.com>
2023-10-23 14:00:39 +0300

    Implement the first automation flow

--- src/main/java/com/opencart/TestRunner.java
+++ src/main/java/com/opencart/TestRunner.java
@@ -1,23 +1,68 @@
 package com.opencart;
 
 import com.opencart.managers.DriverManager;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WindowType;
+import com.opencart.managers.RandomDataManager;
+import org.openqa.selenium.*;
 
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class TestRunner {
     public static void main(String[] args) throws InterruptedException {
-        DriverManager manager = DriverManager.getInstance();
-        WebDriver driver = manager.getDriver();
+
+        WebDriver driver = DriverManager.getInstance().getDriver();
 
         String currentWindowName = driver.getWindowHandle();
 
         driver.switchTo().newWindow(WindowType.TAB);
+        driver.get("https://andreisecuqa.host/");
+      //  Thread.sleep(1000);
 
-        driver.get("https://protv.md/");
+        WebElement myAccountIcon = driver.findElement(By.xpath("//i[@class='fa-solid fa-user']"));
+        myAccountIcon.click();
+
+        WebElement registerOption = driver.findElement(By.xpath("//a[@class='dropdown-item'][normalize-space()='Register']"));
+        registerOption.click();
+
+        System.out.println(driver.getCurrentUrl());
+
+        WebElement firstNameInput = driver.findElement(By.id("input-firstname"));
+        String firstName = RandomDataManager.generateFirstName();
+        firstNameInput.sendKeys(firstName);
+        System.out.println(firstName);
+
+        WebElement lastNameInput = driver.findElement(By.cssSelector("#input-lastname"));
+        String lastName = RandomDataManager.generateLastName();
+        lastNameInput.sendKeys(lastName);
+        System.out.println(lastName);;
+
+        WebElement emailInput = driver.findElement(By.cssSelector("#input-email"));
+        emailInput.sendKeys("boss007@gmail.com");String randomEmail = RandomDataManager.generateRandomEmail();
+        emailInput.sendKeys(randomEmail);
+        System.out.println(randomEmail);
+
+        //WebElement phoneInput = driver.findElement(By.cssSelector("#input-telephone"));
+        //phoneInput.sendKeys("060344520");
+
+        WebElement passwordInput = driver.findElement(By.cssSelector("#input-password"));
+        String password = RandomDataManager.generatePassword();
+        passwordInput.sendKeys(password);
+        System.out.println(password);
+
+        //WebElement confirmPasswordInput = driver.findElement(By.cssSelector("#input-confirm"));
+        //confirmPasswordInput.sendKeys("Parola123!");
+
+        WebElement  termsAndConditionToggleBar = driver.findElement(By.cssSelector("input[value='1'][name='agree']"));
+        JavascriptExecutor je = (JavascriptExecutor) driver;
+        je.executeScript("arguments[0].scrollIntoView(true);", termsAndConditionToggleBar);
+        Thread.sleep(1000);
+        termsAndConditionToggleBar.click();
+
+        WebElement registerBtn = driver.findElement(By.cssSelector("button[type='submit']"));
+        registerBtn.click();
+
         Thread.sleep(5000);
+        System.out.println(driver.getTitle());
         driver.quit();
         }
 
